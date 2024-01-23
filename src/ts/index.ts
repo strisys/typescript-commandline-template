a341c6da80385e95549e673a19d0e83250d63639
Stephen Trudel <strudel@firstkeymortgage.com>
2024-01-23 13:14:08 -0500

    update

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,13 +1,39 @@
 import process from 'process';
 import './env-config';
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
 async function main(): Promise<void> {
-  console.log('write your commandline goodness here');
+  const response = (await processText('what is LangSmith?'));
+  console.log(response);
 }
 
-main().catch((err) => {
-  console.log("error code: ", err.code);
+const processText = async (text: string): Promise<string> => {
+  const prompt = ChatPromptTemplate.fromMessages([
+    ['system', 'You are a world class technical documentation writer.'],
+    ['user', '{input}'],
+  ]);
+
+  const chatModel = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY
+  });
+
+  const chain = prompt.pipe(chatModel).pipe(new StringOutputParser());
+
+  const response = (await chain.invoke({
+    input: text,
+  }));
+
+  console.log(response);
+
+  return response;
+}
+
+const processError = (err: Error): void => {
   console.log("error message: ", err.message);
   console.log("error stack: ", err.stack);
   process.exit(1);
-});
+};
+
+main().catch(processError);
