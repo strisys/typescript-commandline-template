e1099ee11df2ae6ca98c060e4fa4c2d572795676
Stephen Trudel <github@strisys.com>
2024-06-23 12:51:29 -0400

    kepa testing

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,34 @@
+import { KeypaConfigBuilder } from 'keypa';
 import process from 'process';
-import './env-config.js';
 
 async function main(): Promise<void> {
-  console.log('write your commandline goodness here');
+  const environments = ['development', 'production'];
+  const currentEnvironment = environments[0];
+  const builder = KeypaConfigBuilder.configure(environments[0], environments[1]);
+
+  const azureConfig = {
+    keyVaultName: 'kv-keypa-development',
+  }
+
+  const awsConfig = {
+    profile: 'playground',
+  }
+
+  // Arrange
+  builder.get(environments[0]).providers
+    .set('dotenv', {})
+    .set('azure-keyvault', azureConfig)
+    .set('aws-secrets-manager', { ...awsConfig, secrets: `${environments[0]}/keypa/config` });
+
+  builder.get(environments[1]).providers
+    .set('dotenv', {})
+    .set('azure-keyvault', azureConfig)
+    .set('aws-secrets-manager', { ...awsConfig, secrets: `${environments[1]}/keypa/config` });
+
+  const keypa = await builder.initialize(currentEnvironment);
+
+  console.log('AZURE-KEYPA-TEST: ', keypa.get('AZURE-KEYPA-TEST').toJson());
+  console.log('AWS-KEYPA-TEST: ', keypa.get('AWS_KEYPA_TEST').toJson());
 }
 
 main().catch((err) => {
